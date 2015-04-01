6039a58315eeb3f7205c29ad798e5556b7918fb8
mistic100 <contact@git.strangeplanet.fr>
2015-04-01 12:00:04 +0200

    enable code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,20 +38,51 @@ module.exports = function(grunt) {
 
         // qunit test suite
         qunit: {
-            all: ['tests/*.html']
+            all: {
+                options: {
+                    urls: ['tests/index.html?coverage=true'],
+                    noGlobals: true
+                }
+            }
+        },
+
+        // save LCOV files
+        qunit_blanket_lcov: {
+            all: {
+                files: [{
+                    expand: true,
+                    src: ['jQuery.extendext.js']
+                }],
+                options: {
+                    dest: '.coverage-results/all.lcov'
+                }
+            }
+        },
+
+        // coveralls data
+        coveralls: {
+            options: {
+                force: true
+            },
+            all: {
+                src: '.coverage-results/all.lcov',
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-qunit-blanket-lcov');
+    grunt.loadNpmTasks('grunt-coveralls');
 
     grunt.registerTask('default', [
         'uglify'
     ]);
 
     grunt.registerTask('test', [
-        'qunit',
-        'jshint'
+        'jshint',
+        'qunit_blanket_lcov',
+        'qunit'
     ]);
 };
