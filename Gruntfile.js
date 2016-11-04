0769fb9e05c63fa567b7f920438b9ca0ffdf4ec0
mistic100 <contact@git.strangeplanet.fr>
2016-11-04 19:26:03 +0100

    Prepare NPM

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,14 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        banner:
-            '/*!\n'+
-            ' * jQuery.extendext <%= pkg.version %>\n'+
-            ' *\n'+
-            ' * Copyright 2014-<%= grunt.template.today("yyyy") %> Damien "Mistic" Sorel (http://www.strangeplanet.fr)\n'+
-            ' * Licensed under MIT (http://opensource.org/licenses/MIT)\n'+
-            ' *\n'+
-            ' * Based on jQuery.extend by jQuery Foundation, Inc. and other contributors\n'+
-            ' */',
+        banner: '/*!\n' +
+        ' * jQuery.extendext <%= pkg.version %>\n' +
+        ' *\n' +
+        ' * Copyright 2014-<%= grunt.template.today("yyyy") %> Damien "Mistic" Sorel (http://www.strangeplanet.fr)\n' +
+        ' * Licensed under MIT (http://opensource.org/licenses/MIT)\n' +
+        ' *\n' +
+        ' * Based on jQuery.extend by jQuery Foundation, Inc. and other contributors\n' +
+        ' */',
 
         // compress js
         uglify: {
@@ -18,27 +17,22 @@ module.exports = function(grunt) {
                 banner: '<%= banner %>\n',
                 mangle: { except: ['$'] }
             },
-            dist: {
-                files: {
-                    'jQuery.extendext.min.js': [
-                        'jQuery.extendext.js'
-                    ]
-                }
+            lib: {
+                src: 'jQuery.extendext.js',
+                dest: 'jQuery.extendext.min.js'
             }
         },
 
         // jshint tests
         jshint: {
             lib: {
-                files: {
-                    src: ['jQuery.extendext.js']
-                }
+                src: 'jQuery.extendext.js'
             }
         },
 
         // qunit test suite
         qunit: {
-            all: {
+            lib: {
                 options: {
                     urls: ['tests/index.html?coverage=true'],
                     noGlobals: true
@@ -48,14 +42,11 @@ module.exports = function(grunt) {
 
         // save LCOV files
         qunit_blanket_lcov: {
-            all: {
-                files: [{
-                    expand: true,
-                    src: ['jQuery.extendext.js']
-                }],
+            lib: {
                 options: {
                     dest: '.coverage-results/all.lcov'
-                }
+                },
+                src: 'jQuery.extendext.js'
             }
         },
 
@@ -64,7 +55,7 @@ module.exports = function(grunt) {
             options: {
                 force: true
             },
-            all: {
+            lib: {
                 src: '.coverage-results/all.lcov'
             }
         }
